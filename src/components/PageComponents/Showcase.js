ff3d1285aff827f3d6dd65d603b40c7fb05ba7ca
whoisasey <atinachang@gmail.com>
2022-03-25 20:18:06 -0400

    add prev and next post  based onclick id

--- src/components/PageComponents/Showcase.js
+++ src/components/PageComponents/Showcase.js
@@ -1,4 +1,4 @@
-import React, { Fragment } from "react"
+import React, { Fragment, useState } from "react"
 import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import { graphql, useStaticQuery, Link } from "gatsby"
 import { sortedNodes } from "../../helpers"
@@ -9,6 +9,28 @@ const Showcase = ({ name }) => {
     allWpShowcase: { nodes },
   } = useStaticQuery(query)
 
+  const [currentPost, setCurrentPost] = useState(0)
+
+  console.clear()
+  const getPostIndex = id => {
+    setCurrentPost(id)
+  }
+
+  const getNextPost = () => {
+    return nodes[currentPost + 1]
+  }
+
+  const lastPost = nodes[nodes.length - 1]
+  const getPrevPost = () => {
+    // if index = 0, go to last node
+    if (currentPost === 0) {
+      return lastPost
+      // else, return nodes[-1]
+    } else return nodes[currentPost - 1]
+  }
+
+  console.log(getNextPost())
+  console.log(getPrevPost())
   return (
     <Fragment>
       <Seo title={name} />
@@ -24,27 +46,29 @@ const Showcase = ({ name }) => {
                 return second - first
               })
               .map(
-                ({
-                  title,
-                  uri,
-                  databaseId,
-                  showcaseGallery: { imageGallery },
-                }) => {
+                (
+                  { title, uri, databaseId, showcaseGallery: { imageGallery } },
+                  i
+                ) => {
                   const image = getImage(imageGallery.image1.localFile)
 
                   if (imageGallery.image2 !== null)
                     return (
-                      <div className="blocks_gallery_item" key={databaseId}>
-                        <Link to={uri}>
-                          <li>
-                            <figure>
-                              <GatsbyImage image={image} alt={title} />
-                            </figure>
-                          </li>
-                          <div className="showcase_overlay">
-                            <h3>{title}</h3>
-                          </div>
-                        </Link>
+                      <div
+                        className="blocks_gallery_item"
+                        key={i}
+                        onClick={() => getPostIndex(i)}
+                      >
+                        {/* <Link to={uri}> */}
+                        <li>
+                          <figure>
+                            <GatsbyImage image={image} alt={title} />
+                          </figure>
+                        </li>
+                        <div className="showcase_overlay">
+                          <h3>{title}</h3>
+                        </div>
+                        {/* </Link> */}
                       </div>
                     )
 
